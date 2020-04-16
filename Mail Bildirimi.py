44a237389f0d2d43c3aa4535b8d5da17389a0d44
ebubekir07 <ebubekir07@users.noreply.github.com>
2020-04-17 00:25:19 +0300

    Update Mail Bildirimi.py

--- Mail Bildirimi.py
+++ Mail Bildirimi.py
@@ -1,25 +1,3 @@
-"""
-bekirarkali.blogspot.com
-ig: @bekrarkali
-github: https://github.com/ebubekir07/projeler
-https://www.youtube.com/channel/UCfRC3PPhSJDlqjU_me437KA
-
-
-Yazılımın çalışabilmesi için yazılımda gerekli ayarlar yapıldıktan sonra google
-güvenliğinden "DAHA AZ GÜVENLİ UYGULAMA ERİŞİMİ" seçeneğini aktif hale
-getirmeniz gerekiyor. Bu yazılımı uzun süre kullanmadığınız zaman google
-ayarları eski hale getirecektir.
-
-Windows'un zaman görevleyici uygulamasına bu yazılımın python kodunu ekleyerek
-her bilgisayar açıldığında bu yazılımı çalıştırmasını sağlayabilirsiniz.
-Böylece bilgisayar açılır açılmaz internet bağlantısı varsa size mail gelir ve
-bilgisayarın açıldığından haberiniz olur.
-
-Eğer isterseniz maili kendinize de gönderebilirsiniz.
-Kodlar bana ait değildir. 
-"""
-
-
 import smtplib
 from email.mime.text \
 import MIMEText
